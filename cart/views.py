561c4d91a6c121fda97391a5245e88b0297ca9f0
Bryar Osman <bryarosman.bo@gmail.com>
2024-02-23 16:43:01 +0000

    the add to cart is now have basic fuctioning

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_cart(request):
     """ View cart to render users added items """
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping cart """
+    # Use a default quantity of 1 (or another value) if 'quantity' is not provided in the POST data
+    quantity = request.POST.get('quantity')
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if quantity is not None:
+        quantity = int(quantity)
+    else:
+        # Default quantity can be set to 1 or any value that makes sense for your application
+        quantity = 1
+
+    if item_id in cart:
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
