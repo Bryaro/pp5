069634199447159040ed74ee771e78d3412385ee
Bryar Osman <bryarosman.bo@gmail.com>
2024-02-29 20:48:32 +0000

    test review pages

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,7 +1,7 @@
-app_name = 'reviews'
-
 from django.urls import path
 from .views import add_review, all_reviews
+
+app_name = 'reviews'
 
 urlpatterns = [
     path('add/<int:product_id>/', add_review, name='add_review'),
