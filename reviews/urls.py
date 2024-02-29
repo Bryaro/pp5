648f88fc26845106339f046ba81bdacecf6f9a07
Bryar Osman <bryarosman.bo@gmail.com>
2024-02-29 19:56:02 +0000

    look for bug for reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,5 +5,5 @@ from .views import add_review, all_reviews
 
 urlpatterns = [
     path('add/<int:product_id>/', add_review, name='add_review'),
-    path('reviews/', all_reviews, name='all_reviews'),
+    path('', all_reviews, name='all_reviews'),
 ]
