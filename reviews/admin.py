4ca13388029ae04ff5e1555938df12821f13fb5f
Bryar Osman <bryarosman.bo@gmail.com>
2024-02-29 20:17:26 +0000

    removed review images via admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user', 'rating', 'created_at')
+    list_filter = ('rating', 'created_at')
+    search_fields = ('comment', 'product__name', 'user__username')
+
+admin.site.register(Review, ReviewAdmin)
