40e7d7733d4580b8df49f2504b99f32a551dc289
Bryar Osman <bryarosman.bo@gmail.com>
2024-02-29 21:12:12 +0000

    fixed bug for reviews, run heroku migrations and migrated solved the issue

--- cosmos_sweden/settings.py
+++ cosmos_sweden/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key_if_not_set')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-bryaro-pp5-k2tlzmd5ekc.ws-eu108.gitpod.io', 'pp5-bryar-475062670c00.herokuapp.com', 'localhost', '127.0.0.1']
 
