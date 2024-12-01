45be0591ef05a5858a6af93376985490db5dea16
Bryar Osman <bryarosman.bo@gmail.com>
2024-12-01 15:57:44 +0000

    Integrated CKEditor for product description

--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 from django.forms import inlineformset_factory
+from ckeditor.widgets import CKEditorWidget  # Import CKEditorWidget
 from .models import Product, ProductImage
 
 class ProductForm(forms.ModelForm):
     """
     A form for creating and updating Product instances.
     """
+    description = forms.CharField(widget=CKEditorWidget())  # Use CKEditorWidget for description
+
     class Meta:
         model = Product
         fields = '__all__'
