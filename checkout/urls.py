ab7601cbd0abb143fc1889ac24bf0461e225717d
Bryar Osman <bryarosman.bo@gmail.com>
2025-03-16 15:09:28 +0000

    changed back to card payment

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,7 +4,9 @@ from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-    path('checkout_success/', views.checkout_success, name='checkout_success'),
-    path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
+    path('checkout_success/<order_number>',
+         views.checkout_success, name='checkout_success'),
+    path('cache_checkout_data/',
+         views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
