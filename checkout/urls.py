c07ef0bf6a5e1c1b46901f6884c832c3c5b00173
Bryar Osman <bryarosman.bo@gmail.com>
2025-03-16 16:18:02 +0000

    Remove outdated checkout view reference

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,7 +3,6 @@ from . import views
 from .webhooks import webhook
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),
     path('checkout_success/', views.checkout_success, name='checkout_success'),
     path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
     path('wh/', webhook, name='webhook'),
