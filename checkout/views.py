c5255240c6daf6ae5748d55dd1d81e35b035ab80
Bryar Osman <bryarosman.bo@gmail.com>
2025-03-19 18:43:02 +0000

    Fixed worldwide shipping in Stripe checkout by adding valid country codes

--- checkout/views.py
+++ checkout/views.py
@@ -33,8 +33,16 @@ def create_checkout_session(request):
     # Default payment methods (Apple Pay and Google Pay work automatically with "card")
     payment_methods = ["card", "link", "paypal", "mobilepay"]
 
-    # Shipping Address Collection (Allows worldwide shipping)
-    shipping_address_collection = {"allowed_countries": ["*"]}
+    # List of all supported shipping countries
+    shipping_countries = [
+        "AC", "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ",
+        "CA", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CV", "CW", "CY", "CZ", "DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK", "FO", "FR", "GA", "GB", "GD",
+        "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY", "HK", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IM", "IN", "IO", "IQ", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH",
+        "KI", "KM", "KN", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF", "MG", "MK", "ML", "MM", "MN", "MO", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX",
+        "MY", "MZ", "NA", "NC", "NE", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PR", "PS", "PT", "PY", "QA", "RE", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD",
+        "SE", "SG", "SH", "SI", "SJ", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SZ", "TA", "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "US", "UY",
+        "UZ", "VA", "VC", "VE", "VG", "VN", "VU", "WF", "WS", "XK", "YE", "YT", "ZA", "ZM", "ZW", "ZZ"
+    ]
 
     # Add Hand Size as a custom field
     custom_fields = [
@@ -62,7 +70,7 @@ def create_checkout_session(request):
         mode='payment',
         success_url=request.build_absolute_uri(reverse('checkout_success')) + "?session_id={CHECKOUT_SESSION_ID}",
         cancel_url=request.build_absolute_uri(reverse('view_cart')),
-        shipping_address_collection=shipping_address_collection,  # Collects shipping address
+        shipping_address_collection={"allowed_countries": shipping_countries},  # Collects shipping address
         custom_fields=custom_fields  # Adds hand size selection
     )
 
